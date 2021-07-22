ba6dc9ead656040915d3ac864a7eeeb961c4e402
muffin <squishysparrow7.7@gmail.com>
2021-07-23 01:58:49 +1200

    writepbm file

---
+++ writepbm.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "points.h"
+
+/* Example pbm file format from
+ * https://en.wikipedia.org/wiki/Netpbm#PBM_example
+ *
+ * 0 = white
+ * 1 = black
+ */
+
+/*
+ * P1
+ * # This is an example bitmap of the letter "J"
+ * 6 10
+ * 0 0 0 0 1 0
+ * 0 0 0 0 1 0
+ * 0 0 0 0 1 0
+ * 0 0 0 0 1 0
+ * 0 0 0 0 1 0
+ * 0 0 0 0 1 0
+ * 1 0 0 0 1 0
+ * 0 1 1 1 0 0
+ * 0 0 0 0 0 0
+ * 0 0 0 0 0 0
+ */
+
+/* Will write a pbm file out of arguments.
+ * Args:
+ *  > Array of struct points
+ *  > Len of array
+ *
+ * Then it will calculate the width and height of the file.
+ * It will create an empty array of that size, will loop through
+ * the given array setting the specific bits to 1.
+ * It iwll then write it to a pbm file.
+ */
