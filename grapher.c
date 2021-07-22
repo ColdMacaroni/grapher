653e437b8b9e92ea442595a2be3c450b934eb7f9
muffin <squishysparrow7.7@gmail.com>
2021-07-22 23:51:32 +1200

    only getting first coord

--- grapher.c
+++ grapher.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
 typedef
 struct
@@ -29,30 +30,65 @@ point
 inputPoint(void)
 {
     char input[BUFSIZ];
+    int num_len = 0;
+    char *num = malloc(sizeof(char));
+
+    int x = 0;
+    int y = 0;
+
+    int *coord = &x;
 
     fgets(input, BUFSIZ, stdin);
 
     // Go through every character
     // +1 so null character is never considered
+    // Store digits in a char array
     for (int i = 0; input[i + 1] != '\0'; i++)
     {
         // Skip brackets and space in points (x, y) comma to be handled separately
-        if (input[i] == '(' ||
-            input[i] == ')' ||
-            input[i] == ' ')
+        if (!isdigit(input[i]) && input[i] != ',')
             continue;
 
+        if (input[i] == ',')
+        {
+            // Convert to int
+            *coord = strToInt(num, num_len);
 
+            // Reset stuff
+            num_len = 0;
+            num = malloc(sizeof(char));
+
+            // Now store y
+            coord = &y;
+
+            continue;
+        }
+
+        // Store the current digit in the array
+        num[num_len] = input[i];
+
+        // Increase how many numbers there are
+        num_len++;
+
+        // Make space for next number
+        num = realloc(num, (num_len + 1) * sizeof(char));
     }
 
-    point hi;
-    hi.x = 2; hi.y=3;
-    return hi;
+    free(num);
+
+    // Placeholder
+    point pt = {
+        .x = x,
+        .y = y
+    };
+
+    return pt;
 }
 
 int
 strToInt(char *str, int len)
 {
+    /* Convert an array of chars to int */
     int digits = len - 1;
     int num = 0;
 
