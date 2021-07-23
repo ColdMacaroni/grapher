45c060c03a95d2b65a3a546358848e4b8390fdcd
muffin <squishysparrow7.7@gmail.com>
2021-07-23 17:01:00 +1200

    loop_ptr to node_ptr and actually going to next node

--- writepbm.c
+++ writepbm.c
@@ -44,8 +44,8 @@ void writePbm(node_t *head, char *filename)
         exit(EXIT_FAILURE);
     }
 
-    // Sep pointer for looping as to not lose the head.
-    node_t *loop_ptr;
+    // Sep pointer for looping to not lose the head.
+    node_t *node_ptr;
 
     // Initialize min and max variables to first element
     int min_x, max_x;
@@ -60,12 +60,12 @@ void writePbm(node_t *head, char *filename)
 
     // Get minimum and maximum x and y.
     // Will be used for calculating the size of the image
-    loop_ptr = head;
-    while (loop_ptr != NULL)
+    node_ptr = head;
+    while (node_ptr != NULL)
     {
         // Store them for quicker accessing
-        ptr_x = loop_ptr->coord.x;
-        ptr_y = loop_ptr->coord.y;
+        ptr_x = node_ptr->coord.x;
+        ptr_y = node_ptr->coord.y;
 
         /* X */
         if (min_x > ptr_x)
@@ -78,6 +78,9 @@ void writePbm(node_t *head, char *filename)
             min_y = ptr_y;
         else if (max_y < ptr_y)
             max_y = ptr_y;
+
+        // Go to next node
+        node_ptr = node_ptr->next;
     }
 
     printf("\tX\tY\nMin\t%d\t%d\nMax\t%d\t%d\n", min_x, min_y, max_x, max_y);
