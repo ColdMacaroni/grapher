20a08ff65deeee0a683fa7d07a02c9b82d2e5ab0
muffin <squishysparrow7.7@gmail.com>
2021-07-23 12:33:21 +1200

    linked list prototypes

--- points.h
+++ points.h
@@ -9,4 +9,16 @@ _point
     int y;
 } point_t;
 
+// Linked list
+typedef
+struct
+_node
+{
+    point_t coord;
+    struct _node *next;
+} node_t;
+
+node_t createNode(point_t value);
+void insertNode(**head, node_t to_insert);
+
 #endif
